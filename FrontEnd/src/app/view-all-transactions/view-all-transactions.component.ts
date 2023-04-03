373516837b92d1ac8cdf413bf38319dcee403d81
main <eperry910@yahoo.com>
2023-04-03 15:26:53 -0400

    Created view all transaction component

---
+++ FrontEnd/src/app/view-all-transactions/view-all-transactions.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-all-transactions',
+  templateUrl: './view-all-transactions.component.html',
+  styleUrls: ['./view-all-transactions.component.css']
+})
+export class ViewAllTransactionsComponent {
+
+}
