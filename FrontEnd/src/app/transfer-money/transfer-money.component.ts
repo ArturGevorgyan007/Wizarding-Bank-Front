1c397d2bc9f15d75505724f90ac84fb2568644a1
Gibsonchan98 <59757387+Gibsonchan98@users.noreply.github.com>
2023-04-04 15:54:55 -0700

    need to check why service is not working

--- FrontEnd/src/app/transfer-money/transfer-money.component.ts
+++ FrontEnd/src/app/transfer-money/transfer-money.component.ts
@@ -1,16 +1,24 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { Observable } from 'rxjs';
+import { FormControl } from '@angular/forms';
+import { TransferService } from '../transfer.service';
 
 @Component({
   selector: 'app-transfer-money-component',
   templateUrl: './transfer-money.component.html',
   styleUrls: ['./transfer-money.component.css']
 })
-export class TransferMoneyComponent {
+export class TransferMoneyComponent{
+
+  // constructor(private service : TransferService){}
 
   display = true;
   displayAdd = false;
   displayFinal = false;
   _amount : any = 0;
+  cardDsiplay = true;
+  bankDisplay = false;
+  type : any = "c";
 
   addToWallet() {
     this.display = false;
@@ -21,5 +29,25 @@ export class TransferMoneyComponent {
     this.displayAdd = false;
     this.displayFinal = true;
     this._amount = amount;
+  }
+
+  change(){
+    console.log("should show card or account options");
+  }
+
+  setType(type : any){
+    if(type == "b"){
+      this.type = "b";
+      console.log("with bank", this.type);
+    } else {
+      this.type = "c";
+    }
+  }
+  
+  addMoney(){
+    console.log(this._amount, this.type, "user id: 1");
+    // this.service.cardToWallet(1,1,1).subscribe(data => {
+    //   console.log(data);
+    // })
   }
 }
---
+++ FrontEnd/src/app/transfer.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Transaction } from './models/transaction';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TransferService {
+
+  apiRoot: string = 'http://localhost:5070/';
+
+  constructor(private http: HttpClient) {}
+  //When sending money to self, recipient = id, sender = null
+  //TODO: wrap functions to accept name email etc
+  //Card-to-wallet
+  cardToWallet(cardId : number, userId : number, amount : number) : Observable<any>{
+    var body : Transaction = {
+      "cardId": cardId,
+      "recipientId": userId,
+      "amount": amount
+    };
+    return this.http.post(this.apiRoot + 'Transaction', body) as Observable<any>; 
+  }
+  
+  //wallet-to-card
+  walletToCard(userId : number, cardId : number, amount : number) : Observable<any> {
+    var body : Transaction = {
+      "senderId": userId,
+      "cardId": cardId,
+      "amount": amount
+    };
+    return this.http.post(this.apiRoot + 'Transaction', body) as Observable<any>; 
+  }
+
+  //bankAccount-to-wallet
+  accountToWallet(accountId : number, userId : number, amount : number) : Observable<any> {
+    var body : Transaction = {
+      "accountId": accountId,
+      "recipientId": userId,
+      "amount": amount
+    };
+    return this.http.post(this.apiRoot + 'Transaction', body) as Observable<any>; 
+  }
+
+  //wallet-to-bankAccount
+  walletToAccount(userId : number, accountId : number, amount : number) : Observable<any> {
+    var body : Transaction = {
+      "accountId": accountId,
+      "senderId": userId,
+      "amount": amount
+    };
+    return this.http.post(this.apiRoot + 'Transaction', body) as Observable<any>; 
+  }
+  
+}
