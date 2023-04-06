5885965fb4a4fbba9b19c4ac22e7aa96569969ab
main <eperry910@yahoo.com>
2023-04-06 10:56:18 -0400

    Added logout functionality

--- FrontEnd/src/app/navbar/navbar.component.ts
+++ FrontEnd/src/app/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { UserDataService } from '../user-data.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-
+  constructor(private cookieService: CookieService, private authService: AuthService, private userData: UserDataService) { }
+  Logout(): void {
+    this.cookieService.set('userType', '')
+    this.cookieService.set('email', '')
+    this.authService.logout()
+  }
 }
