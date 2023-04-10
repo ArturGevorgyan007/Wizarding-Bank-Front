c35b64fef07526932f699854e7d41333b775ed5d
main <eperry910@yahoo.com>
2023-04-10 16:27:46 -0400

    Updated Auth0

--- FrontEnd/src/app/Landing/Landing.component.ts
+++ FrontEnd/src/app/Landing/Landing.component.ts
@@ -51,12 +51,12 @@ export class LandingComponent implements OnInit {
   async login() {
     await this.authService.logout()
     this.cookieService.set('userType', 'Personal', 0.4);
-    this.authService.loginWithRedirect({ authorizationParams: { redirect_uri: 'https://brave-mud-0bd752310.2.azurestaticapps.net/', target: 'Personal' } })
+    this.authService.loginWithRedirect({ authorizationParams: { redirect_uri: 'https://brave-mud-0bd752310.2.azurestaticapps.net/' }, appState: { target: 'Personal' } })
 
   }
   async businessLogin() {
     await this.authService.logout();
     this.cookieService.set('userType', 'Business', 0.4)
-    this.authService.loginWithRedirect({ authorizationParams: { redirect_uri: 'https://brave-mud-0bd752310.2.azurestaticapps.net/', target: 'Business' } })
+    this.authService.loginWithRedirect({ authorizationParams: { redirect_uri: 'https://brave-mud-0bd752310.2.azurestaticapps.net/' }, appState: { target: 'Business' } })
   }
 }
