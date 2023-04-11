c036e70cc28d6ec49652c98dbc70bc4a98e99492
Gibsonchan98 <59757387+Gibsonchan98@users.noreply.github.com>
2023-04-11 14:40:03 -0700

    transfer-page component works for business now

--- FrontEnd/src/app/transfer-page/transfer-page.component.ts
+++ FrontEnd/src/app/transfer-page/transfer-page.component.ts
@@ -14,30 +14,47 @@ import { CookieService } from '../../../node_modules/ngx-cookie-service';
 })
 export class TransferPageComponent implements OnInit{
   mode : string = "card";
-  constructor(private cookieserive : CookieService, private router: Router, private fb: FormBuilder, private api:TransferService, private userData: UserDataService){}
-  UID = parseInt(this.cookieserive.get('userId')) 
+  constructor(private cookieService : CookieService, private router: Router, private fb: FormBuilder, private api:TransferService, private userData: UserDataService){}
   
+  UID : any; 
   transaction : string;
   cards : any[];
-  banks : any[];
+  banks : any[] = [];
   walletamt : number;
   showselected : boolean = false;
   selected : string;
-
+  acctType : any;
 
   ngOnInit(): void {
-
+   // this.UID = parseInt(this.cookieService.get('userId')) 
+   this.UID = 3;
+    this.acctType = this.cookieService.get('userType');
     //get all cards
-    this.userData.getUserCards(this.UID)?.subscribe((data:any) => {
-      this.cards = data
-    })
-
-      //get all bank accounts
-      this.userData.getUserAccounts(this.UID)?.subscribe((data:any) => {
-        this.banks = data
+    if(this.acctType == 'Business'){
+      this.userData.getBusinessCards(this.UID).subscribe((data:any)=> {
+        this.cards = data; 
       })
-
+      this.userData.getBankAccounts().subscribe(data =>{
+        if(data != null){
+          for(let i = 0; i < data.length; i++){
+            if(data[i]['businessId'] == this.UID){
+              this.banks.push(data[i]);
+            }
+          }
+        }
+      })
+    } 
+    else{
+      this.userData.getUserCards(this.UID)?.subscribe((data:any) => {
+        this.cards = data
+      })
+  
+        //get all bank accounts
+        this.userData.getUserAccounts(this.UID)?.subscribe((data:any) => {
+          this.banks = data
+        })
     }
+  }
 
 
 
@@ -69,21 +86,36 @@ export class TransferPageComponent implements OnInit{
     if(this.cardForm.valid) {
       let A = this.cardForm.value['cardAmount'];
       let C = parseInt(this.selected)
+      
+      if(this.acctType == 'Business'){
+        //get wallet balance
+        this.userData.getWalletBBalance(this.UID).subscribe(data => {
+          if (data == null ){return}
+          else{
+            this.walletamt = data[0]['wallet']; 
+            console.log(this.walletamt + "lesssss" + A);
+            if(this.walletamt >= A){
+              console.log(this.walletamt + "lesssss" + A);
+              this.api.walletToCard(this.UID, C, A, true).subscribe(data => {if(data != null) this.transaction = data['amount']});
 
-
-           //get wallet balance
-           this.userData.getWalletBalance(this.UID).subscribe(data => {
+            }
+            else alert('Not enough money')
+          }     
+        });
+      } else{
+          //get wallet balance
+          this.userData.getWalletBalance(this.UID).subscribe(data => {
             if (data == null ){return}
             else{
               this.walletamt = data['wallet']; 
               if(this.walletamt >= A){
-                this.api.walletToCard(this.UID, C, A, true).subscribe(data => {if(data != null) this.transaction = data['amount']});
+                this.api.walletToCard(this.UID, C, A, false).subscribe(data => {if(data != null) this.transaction = data['amount']});
 
               }
               else alert('Not enough money')
             }     
           });
-
+      }
     }
   }
 
@@ -95,24 +127,40 @@ export class TransferPageComponent implements OnInit{
       let A = this.bankForm.value['bankAmount']; 
       let B = parseInt(this.selected)   
 
-      // get wallet balance
-      this.userData.getWalletBalance(this.UID).subscribe(data => {
-        if (data == null) {
-          return; // add null check
-        } else {
-          this.walletamt = data['wallet']; 
-          if (this.walletamt >= A) {
-            this.api.walletToAccount(this.UID, B, A, true).subscribe(data => {
-              if (data != null) this.transaction = data['amount'];
-            });
+      if(this.acctType == 'Business'){
+          // get wallet balance
+        this.userData.getWalletBBalance(this.UID).subscribe(data => {
+          
+          if (data == null) {
+            return; // add null check
           } else {
-            alert('Not enough money');
+            this.walletamt = data[0]['wallet']; 
+            if (this.walletamt >= A) {
+              this.api.walletToAccount(this.UID, B, A, true).subscribe(data => {
+                if (data != null) this.transaction = data['amount'];
+              });
+            } else {
+              alert('Not enough money');
+            }
           }
-        }
-      });
-
-      
-
+        });
+      } else{
+        // get wallet balance
+        this.userData.getWalletBalance(this.UID).subscribe(data => {
+          if (data == null) {
+            return; // add null check
+          } else {
+            this.walletamt = data['wallet']; 
+            if (this.walletamt >= A) {
+              this.api.walletToAccount(this.UID, B, A, false).subscribe(data => {
+                if (data != null) this.transaction = data['amount'];
+              });
+            } else {
+              alert('Not enough money');
+            }
+          }
+        });
+      }
     }
 
   }
